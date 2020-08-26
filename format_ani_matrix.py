df1690181d5e8532beae0cab410665b54948918b
Brittany Suttner <41094589+brittanysuttner@users.noreply.github.com>
2020-08-26 16:48:29 -0400

    Update format_ani_matrix.py

--- format_ani_matrix.py
+++ format_ani_matrix.py
@@ -1,5 +1,9 @@
+#!/usr/local/pacerepov1/python/3.6/bin/python
 ## This takes outfile from aai.rb with the -T option and creates an AII matrix ##
 # for i in *gz; do for j in *gz; do aai.rb -1 $i -2 $j -T ${i%%.*}-${j%%.*}.out ;done; done
+
+### Takes as input a list of the MAG names obtained from echo ${i%%.*} ####
+### don't forget to change the names of the MAGs file and the output Matrix as needed ###
 
 import csv
 import pandas as pd
@@ -10,27 +14,31 @@ from os.path import isfile, join
 
 #Initialize empty dict and df
 data={}
-#Get a list of the 96 MAGs. Can be a text file that I import or extract that list somehow.
-MAGs = ["cow9_15", "pig8_7"]
+#Import text file that has list of all the MAGs.
+MAGs = []
+with open('MAGs_for_python.txt') as MAGfile:
+   for line in csv.reader(MAGfile, dialect="excel-tab"):
+      MAGs.append(line[0])
+#print(MAGs)
 df_matrix = pd.DataFrame(data, columns = MAGs, index = MAGs)
+print(df_matrix.head())
 
 
-#make a list of all files
+#make a list of all files that end in ".out"
 mypath = '.'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) if ".out" in f]
 
-#TODO I would like to obtain my path from the argument in the CLI  
-
-# make a matrix directly:
 for filename in onlyfiles:
   #obtain arg1 and arg2 from fileName
   filename_rm_out = filename.split(".", 1)[0]
   MAG_names = filename_rm_out.split("-")
-  with open(filename) as tsv: 
+  #print(MAG_names)
+#Make matrix directly
+  with open(filename) as tsv:
      for line in csv.reader(tsv, dialect="excel-tab"):
-       value= line
+       value = line
        df_matrix[MAG_names[0]][MAG_names[1]]=value[0]
-       
-       
+print(df_matrix.head())
+
 #write arg1, arg2, and value to dataframe
-df_matrix.to_csv("Fecal_MAGs_AAI_matrix.txt", index=True, sep='\t')
+df_matrix.to_csv("Fecal_D7_MAGs_AAI_matrix.txt", index=True, sep='\t')
